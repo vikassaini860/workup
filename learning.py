c26d3654a4eae66aeaa6c93a86404080a62cc020
vikasaini <vikas.saini@msg.ai>
2019-09-28 01:22:00 +0530

    checking comment adding

--- learning.py
+++ learning.py
@@ -2,7 +2,7 @@
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-
+#adding comment
 query = {
     "size": 0,
     "query": {
